540ffbb5135208c7b77f6df73a148c00b050da56
Mike_Schaid <68658862+Mschaid@users.noreply.github.com>
2022-06-01 11:00:11 -0500

    refactoring

--- pavlovian_functions.py
+++ pavlovian_functions.py
@@ -3,9 +3,11 @@ import pandas as pd
 import h5py
 import os
 import glob
+import re
 
 
 def create_new_dir(file_path, new_dir_ext):
+    """ creates new directory from file path and extension"""
     new_directory = os.path.join(file_path, new_dir_ext)
     if not os.path.exists(new_directory):
         os.mkdir(new_directory)
@@ -14,22 +16,26 @@ def create_new_dir(file_path, new_dir_ext):
         print('directroy already exists')
 
 
-def find_ts_file_path(file_path, file_basename):
+def find_file_path(file_path, file_basename):
+    """ find full path when given filepath and basename of file"""
     for root, dirs, files in os.walk(file_path):
         if file_basename in files:
             return os.path.join(root, file_basename)
 
 
 def read_timestamps(file_path):
+    """ reads raw timesstamp files from hdf5 guppy foles"""
     ts_arr = h5py.File(file_path, "r").get('timestamps')
     return np.array(ts_arr)
 
 
 def list_subdirs(directory):
+    """ given directory, returns list of all sub dirs as full path"""
     return [os.path.join(directory, file) for file in os.listdir(directory)]
 
 
 def align_events(df, event, event_align):
+    """ function to create PSTH by aligning events to specific event, given a dataframe"""
     event_arr = df[event][np.where(df[event] != 0)[0]].dropna()
     event_align_arr = df[event_align].dropna().to_numpy()
     align_events_dict = {}
@@ -42,6 +48,7 @@ def align_events(df, event, event_align):
 
 
 def calc_frequency(filepath):
+    """ calculate frequency of events around specifc EPOCH"""
     # time is in seconds
     df = pd.read_csv(filepath)  # read csv
     arr = df[(df > -10) & (df < 20)].to_numpy()  #
@@ -52,6 +59,101 @@ def calc_frequency(filepath):
 
 
 def flatten_df(df):
+    """ flattens column to tidy data after groupby and aggreation"""
     df.columns = df.columns = ['_'.join(col) for col in df.columns.values]
     df = df.reset_index()
     return df
+
+
+def create_behavior_dataframe(recording: str, filepath_list):  # recording: str,
+    """ function to calculate mean frequency of event and convert to dataframe"""
+
+    freq_dict = {}
+    for f in filepath_list:
+        day_numb = (re.search(('Day_\d'), f)[0]).split('_')[-1]
+        mouse_id = os.path.basename(f).split("_")[0]
+        freq_arr = calc_frequency(f)
+        freq_dict[mouse_id] = freq_arr
+        if len(freq_dict.keys()) != len(filepath_list):
+            pass
+        else:
+            df = pd.DataFrame.from_dict(freq_dict)
+            # create dataframe from filepath and pair with event name
+            group_behavior_df = (
+                df
+                .rolling(window=2, center=True).mean()
+                .assign(
+                    # mean=df.mean(axis=1),  # add mean,
+                    time_sec=np.arange(-10, 21, 0.2),  # add time column
+                    recording=recording,
+                    day=(day_numb)
+                )
+                .dropna()
+                .melt(id_vars=['day', 'time_sec', 'recording'], var_name='mouse', value_name='avg_frequency')
+            )
+
+    return recording, group_behavior_df
+
+
+def collect_fp_data(path, sensor_regex, region_regex, event_regex):
+    """function to collect photometry data and format for tidy format"""
+
+    day_numb = (re.search(('Day_\d'), path)[0]).split('_')[-1]
+    sensor = re.search(sensor_regex, os.path.basename(path))[0]
+    region = re.search(region_regex, os.path.basename(path))[0]
+    event = re.search(event_regex, os.path.basename(path))[0]
+
+    df = pd.read_hdf(path)
+    time_stamps = df['timestamps']
+    df_clean = (
+        df
+        .drop(['timestamps'], axis=1)
+        .rolling(window=500, center=True).mean()
+        .assign(day=day_numb)
+        .rename(columns=lambda c: c.split('-')[0])
+        .drop(columns=['err'])
+        .assign(time_stamps=time_stamps)
+        .dropna()
+        .melt(id_vars=['day', 'time_stamps'], var_name='mouse', value_name='z-score')
+        .assign(sensor=sensor,
+                region=region,
+                event=event,
+                recording='photometry')
+    )
+    return df_clean
+
+
+def collect_AUC_data(path, sensor_regex, region_regex, event_regex):
+    """function to collect AUC and peak data -> format for tidy format"""
+
+    day_numb = (re.search(('Day_\d'), path)[0]).split('_')[-1]
+    sensor = re.search(sensor_regex, os.path.basename(path))[0]
+    region = re.search(region_regex, os.path.basename(path))[0]
+    event = re.search(event_regex, os.path.basename(path))[0]
+
+    df = pd.read_hdf(path)
+
+    df = pd.read_csv(path)
+    AUC_clean = (
+        df
+        .assign(mouse=df['Unnamed: 0'].str.split('-', expand=True)[0],
+                trial=df['Unnamed: 0'].str.split('_', expand=True)[1])
+        .drop(columns='Unnamed: 0')
+        .melt(id_vars=['mouse', 'trial'], var_name='category', value_name='value')
+        .assign(sensor=sensor,
+                region=region,
+                event=event,
+                day=day_numb,
+                recording='photometry')
+    )
+    return AUC_clean
+
+
+def drop_mice(df):  # filteres dataframe prior to group and aggreation
+
+    filtered_df = df[(df.mouse != '512581') &
+                     (df.mouse != '514957') &
+                     #  (df.mouse != '514958') &
+                     (df.mouse != 'mean')
+                     ]
+    return filtered_df
